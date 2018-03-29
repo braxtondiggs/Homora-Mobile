9466110814c50e5cb132eb2b0b48ca57ab62ebec
Braxton Diggs <braxtondiggs@gmail.com>
2018-03-29 17:46:56 -0400

    Add chat message

--- src/pages/messages/message/message.ts
+++ src/pages/messages/message/message.ts
@@ -1,7 +1,42 @@
 import { Component } from '@angular/core';
+import { NavParams } from 'ionic-angular';
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import { Listing, Message, User } from '../../../interface';
+import { UserProvider } from '../../../providers';
+import { findKey } from 'lodash';
 
 @Component({
   selector: 'page-message',
   templateUrl: 'message.html',
 })
-export class MessagePage { }
+export class MessagePage {
+  key: string;
+  user: User;
+  receiver: User;
+  receiver$: Observable<User>
+  message$: Observable<Message>;
+  private messageDoc: AngularFirestoreDocument<Message>;
+  constructor(private navParams: NavParams,
+    private afs: AngularFirestore,
+    private userProvider: UserProvider) { }
+
+  ionViewDidLoad() {
+    this.key = this.navParams.get('key');
+    if (this.key) {
+      this.user = this.userProvider.get();
+      this.messageDoc = this.afs.doc<Message>(`Messages/${this.key}`);
+      this.message$ = this.messageDoc.snapshotChanges().map((action: any) => {
+        const data = action.payload.data();
+        const userRef = findKey(data.users, (o, key) => this.user.$key !== key);
+        this.receiver$ = this.afs.doc<User>(`Users/${userRef}`).snapshotChanges().map((action: any) => ({ $key: action.payload.id, ...action.payload.data() }));
+        this.receiver$.subscribe((receiver) => {
+          console.log(receiver);
+          this.receiver = receiver;
+        });
+        console.log(data);
+        return ({ $key: action.payload.id, ...data });
+      });
+    }
+  }
+}
