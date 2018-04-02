488472910163a4ff3a48785c476a46b1f139e9cb
Braxton Diggs <braxtondiggs@gmail.com>
2018-04-02 18:28:50 -0400

    Start filter section

--- src/components/filter/filter.ts
+++ src/components/filter/filter.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { ViewController } from 'ionic-angular';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import { Listing } from '../../interface';
+import { ListingProvider } from '../../providers';
+import { filter } from 'lodash';
 
 @Component({
   selector: 'filter',
   templateUrl: 'filter.html'
 })
 export class FilterComponent {
-  constructor(private view: ViewController) { }
+  sort: string = 'Best Match';
+  price: { lower: number, upper: number } = { lower: 0, upper: 10000 };
+  duration: { lower: number, upper: number } = { lower: 0, upper: 12 };
+  gender: string = 'all';
+  groupEarly20: boolean = true;
+  groupLate20: boolean = true;
+  group30: boolean = true;
+  group40older: boolean = false;
+  listings$: Observable<Listing[]>;
+  private listingsCollection: AngularFirestoreCollection<Listing>;
+  constructor(private afs: AngularFirestore,
+    private listingProvider: ListingProvider,
+    private view: ViewController) {
+    this.initComponent();
+  }
 
   close() {
     this.view.dismiss({});
@@ -16,7 +35,24 @@ export class FilterComponent {
     //TODO: Add reset all
   }
 
-  ionViewDidLoad() {
+  onChange() {
+    this.listingsCollection = this.afs.collection<Listing>('Listings', (ref) => ref.where('status', '==', 'published'));
+    this.listings$ = this.listingsCollection.snapshotChanges().map((actions: any) => {
+      return filter(actions.map((action: any) => {
+        const data = action.payload.doc.data();
+        return ({ $key: action.payload.doc.id, ...data });
+      }), (o) => {
+        return this.listingProvider.filterPrice(o, this.price) &&
+          this.listingProvider.filterDuration(o, this.duration) &&
+          this.listingProvider.filterGender(o, this.gender);
+      });
+    });
+    this.listings$.subscribe((value: any) => {
+      console.log(value);
+    });
+  }
+
+  private initComponent() {
 
   }
 }
--- src/providers/listing/listing.ts
+++ src/providers/listing/listing.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Listing } from '../../interface';
 
 @Injectable()
 export class ListingProvider {
@@ -10,5 +11,17 @@ export class ListingProvider {
 
   setActive(key: string): void {
     this.active = key;
+  }
+
+  filterPrice(listing: Listing, price: any): boolean {
+    return listing.price >= price.lower && listing.price <= price.upper
+  }
+
+  filterDuration(listing: Listing, duration: any): boolean {
+    return listing.duration && listing.duration.lower >= duration.lower && listing.duration.upper <= duration.upper
+  }
+
+  filterGender(listing: Listing, gender: string): boolean {
+    return gender === 'all' || listing.roommate && listing.roommate.gender === gender || listing.roommate && listing.roommate.gender === 'all';
   }
 }
