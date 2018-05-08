479f17d41610e90ec6b26c3478c6e34c9d93e7f4
Braxton Diggs <braxtondiggs@gmail.com>
2018-05-08 17:48:37 -0400

    Add basic push notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,38 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
+import { Push, PushObject, PushOptions } from '@ionic-native/push';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { AuthProvider } from '../providers/auth/auth';
 import { IntroPage } from '../pages/intro/intro';
 import { MainPage } from '../pages/main'
+import { FcmProvider } from '../providers';
+import { tap } from 'rxjs/operators';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage: any;
-  constructor(auth: AuthProvider, platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(private fcm: FcmProvider,
+    private push: Push,
+    auth: AuthProvider,
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen) {
     this.rootPage = auth.showIntro() ? IntroPage : MainPage;
     platform.ready().then(() => {
+      fcm.getToken();
       statusBar.styleDefault();
       splashScreen.hide();
+      this.setupPushNotifications();
     });
+  }
+
+  private setupPushNotifications() {
+    this.push.hasPermission().then((res: any) => { });
+    this.fcm.listenToNotifications().pipe(tap((msg) => {
+      console.log(msg);
+    })).subscribe()
   }
 }
--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,4 +1,5 @@
 export * from './auth/auth';
+export * from './fcm/fcm';
 export * from './geolocation/geolocation';
 export * from './listing/listing';
 export * from './user/user';
