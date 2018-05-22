3864608212aeab8c6cd36d86cbaacec50a9247ee
Braxton Diggs <braxtondiggs@gmail.com>
2018-05-22 15:37:36 -0400

    Remove push notifications & add keystore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,40 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import { Push } from '@ionic-native/push';
+// import { Push } from '@ionic-native/push';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { AuthProvider } from '../providers/auth/auth';
 import { IntroPage } from '../pages/intro/intro';
 import { MainPage } from '../pages/main'
-import { FcmProvider } from '../providers';
-import { tap } from 'rxjs/operators';
+// import { FcmProvider } from '../providers';
+// import { tap } from 'rxjs/operators';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage: any;
-  constructor(private fcm: FcmProvider,
-    private push: Push,
-    private platform: Platform,
+  constructor(// private fcm: FcmProvider,
+    // private push: Push,
+    platform: Platform,
     auth: AuthProvider,
     statusBar: StatusBar,
     splashScreen: SplashScreen) {
     this.rootPage = auth.showIntro() ? IntroPage : MainPage;
     platform.ready().then(() => {
-      fcm.getToken();
+      // fcm.getToken();
       statusBar.styleDefault();
       splashScreen.hide();
-      this.setupPushNotifications();
+      // this.setupPushNotifications();
     });
   }
 
-  private setupPushNotifications() {
+  /*private setupPushNotifications() {
     if (this.platform.is('cordova')) {
       this.push.hasPermission().then((res: any) => { });
       this.fcm.listenToNotifications().pipe(tap((msg) => {
         console.log(msg);
       })).subscribe();
     }
-  }
+  }*/
 }
