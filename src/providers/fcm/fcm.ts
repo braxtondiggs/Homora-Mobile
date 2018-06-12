9ca7e04190a87d0984f11a050719b828bc11a575
Braxton Diggs <braxtondiggs@gmail.com>
2018-06-12 01:22:19 -0400

    Add FCM provider

--- src/providers/fcm/fcm.ts
+++ src/providers/fcm/fcm.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Firebase } from '@ionic-native/firebase';
 import { Platform } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
 import { AngularFirestore } from 'angularfire2/firestore';
 
 @Injectable()
@@ -22,23 +23,28 @@ export class FcmProvider {
       token = await this.firebaseNative.getToken();
       await this.firebaseNative.grantPermission();
     }
+
+    if (!this.platform.is('cordova')) {
+      //TODO: Add PWA support with angularfire2
+    }
+
     return this.saveTokenToFirestore(token);
   }
 
   // Save the token to firestore
-  private saveTokenToFirestore(token) {
+  private saveTokenToFirestore(token: string): Promise<void> {
     if (!token) return;
     const devicesRef = this.afs.collection('devices');
     const docData = {
       token,
-      userId: 'testUser'
+      userId: 'testUser' //TODO: change to user auth id
     };
 
     return devicesRef.doc(token).set(docData);
   }
 
   // Listen to incoming FCM messages
-  listenToNotifications() {
+  listenToNotifications(): Observable<any> {
     return this.firebaseNative.onNotificationOpen();
   }
 }
