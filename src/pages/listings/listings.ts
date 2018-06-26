9be9fcadde4602973e40ca691be3daa9284f1c9e
Braxton Diggs <braxtondiggs@gmail.com>
2018-06-26 13:04:12 -0400

    Multiple small fixes #54, fixes #55 & fixes #56

--- src/pages/listings/listings.ts
+++ src/pages/listings/listings.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
-import { LoadingController, ModalController, NavController } from 'ionic-angular';
+import { AlertController, LoadingController, Loading, ModalController, NavController, Platform, ToastController } from 'ionic-angular';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { DocumentReference } from '@firebase/firestore-types';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation';
 import { FilterComponent, MapsComponent } from '../../components';
 import { Observable } from 'rxjs/Observable';
 import { Favorite, Listing, User } from '../../interface';
-import { ListingProvider, UserProvider } from '../../providers';
+import { GeoLocationProvider, ListingProvider, UserProvider } from '../../providers';
 import { ListingPage } from './listing/listing';
 import { ProfileViewPage } from '../profile';
 import { AppSettings } from '../../app/app.constants';
 import * as moment from 'moment';
 import * as _ from 'lodash';
+import geolib from 'geolib';
 
 @Component({
   selector: 'listings',
@@ -28,10 +30,15 @@ export class ListingsPage {
   private favoriteCollection: AngularFirestoreCollection<Favorite>;
   constructor(private afs: AngularFirestore,
     private listingProvider: ListingProvider,
+    private locationProvider: GeoLocationProvider,
     private userProvider: UserProvider,
+    private geolocation: Geolocation,
     private loading: LoadingController,
     private modal: ModalController,
-    private nav: NavController) { }
+    private nav: NavController,
+    private platform: Platform,
+    private alert: AlertController,
+    private toast: ToastController) { }
 
   viewListing(key: string): void {
     this.nav.push(ListingPage, { key });
@@ -91,12 +98,23 @@ export class ListingsPage {
     return moment(date).isSameOrBefore(moment(), 'day') ? 'Available Now' : moment(date).format('MM/DD');
   }
 
-  ionViewDidLoad() {
-    const loader = this.loading.create({ content: 'Finding Listings...' });
-    loader.present();
-    this.isLoading = true;
-    this.user = this.userProvider.get();
-    this.listings$ = this.listingProvider.getListings();
+  getListings(location: Geoposition, loader: Loading) {
+    if (!_.isNull(location) && !this.hasPassedBoundaries(location)) {
+      this.listings$ = this.listingProvider.getListings(false, {
+        center: {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude
+        },
+        radius: 27
+      });
+    } else {
+      this.listings$ = this.listingProvider.getListings();
+      this.alert.create({
+        title: 'Outside supported boundaries',
+        subTitle: 'We currently do not support areas outside of DC, we plan to expand shortly.',
+        buttons: ['Ok']
+      }).present();
+    }
     this.listings$.subscribe((listings: Listing[]) => {
       this.listings = listings;
       loader.dismiss();
@@ -105,6 +123,38 @@ export class ListingsPage {
         this.favoriteCollection = this.afs.collection<Favorite>('Favorites', (ref) => ref.where('user', '==', this.userProvider.getDoc().ref));
         this.favoriteCollection.snapshotChanges().map((actions: any) => actions.map((action: any) => ({ $key: action.payload.doc.id, ...action.payload.doc.data() }))).subscribe((favorites: Favorite[]) => {
           this.favorites = favorites;
+        });
+      }
+    });
+  }
+
+  hasPassedBoundaries(location: Geoposition): boolean {
+    return !geolib.isPointInside({ latitude: location.coords.latitude, longitude: location.coords.longitude }, AppSettings.MAP_BOUNDS)
+  }
+
+  ionViewDidLoad() {
+    const loader: Loading = this.loading.create({ content: 'Finding Listings...' });
+    loader.present();
+    this.isLoading = true;
+    this.user = this.userProvider.get();
+    this.platform.ready().then(() => {
+      if (this.platform.is('cordova')) {
+        this.geolocation.getCurrentPosition({
+          maximumAge: 3000,
+          timeout: 5000,
+          enableHighAccuracy: true
+        }).then((location: Geoposition) => {
+          this.getListings(location, loader);
+        }).catch((error: any) => {
+          this.toast.create({ message: error.toString(), duration: 3000 }).present();
+          this.getListings(null, loader);
+        });
+      } else {
+        this.locationProvider.getLocation().subscribe((location: Geoposition) => {
+          this.getListings(location, loader);
+        }, (error: any) => {
+          this.toast.create({ message: error.toString(), duration: 3000 }).present();
+          this.getListings(null, loader);
         });
       }
     });
