d4585e6d88ab0ff759d3d5023e8587f7f7bad528
Braxton Diggs <braxtondiggs@gmail.com>
2018-11-27 11:47:09 -0500

    Update alert message & improve geolocation fallback

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { Platform } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { AuthProvider } from '../providers/auth/auth';
 import { IntroPage } from '../pages/intro/intro';
@@ -12,12 +13,16 @@ import { MainPage } from '../pages/main';
 export class MyApp {
   rootPage: any;
   constructor(afs: AngularFirestore,
+    geolocation: Geolocation,
     auth: AuthProvider,
     platform: Platform,
     splashScreen: SplashScreen) {
     afs.app.firestore().settings({ timestampsInSnapshots: true });
     this.rootPage = auth.showIntro() ? IntroPage : MainPage;
-    platform.ready().then(() => {
+    Promise.all([
+      geolocation.getCurrentPosition(),
+      platform.ready()
+    ]).then(() => {
       splashScreen.hide();
     });
   }
